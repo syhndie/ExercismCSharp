3753e0f83cd92bf4b7920b389388d57f673654c6
Cindy <syhndie2@gmail.com>
2018-04-10 16:52:42 -0500

    Binary Search exercise

--- binary-search/BinarySearch.cs
+++ binary-search/BinarySearch.cs
@@ -1,14 +1,38 @@
 ﻿using System;
+using System.Linq;
 
 public class BinarySearch
 {
     public BinarySearch(int[] input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        OrderedArray = input.OrderBy(i => i).ToArray();
     }
 
     public int Find(int value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int low = 0;
+        int high = OrderedArray.Length - 1;
+        int mid = (low + high) / 2;
+
+        while (low <= high)
+        {
+            if (value == OrderedArray[mid])
+            {
+                return mid;
+            }
+            if (value < OrderedArray[mid])
+            {
+                high = mid - 1;
+                mid = (low + high) / 2; 
+            }
+            else if (value > OrderedArray[mid])
+            {
+                low = mid + 1;
+                mid = (low + high) / 2;
+            }
+        }
+        return -1;
     }
+
+    private int[] OrderedArray { get; set; }
 }
--- binary-search/BinarySearchTest.cs
+++ binary-search/BinarySearchTest.cs
@@ -12,7 +12,7 @@ public class BinarySearchTest
         Assert.Equal(0, sut.Find(6));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Finds_a_value_in_the_middle_of_an_array()
     {
         var array = new[] { 1, 3, 4, 6, 8, 9, 11 };
@@ -20,7 +20,7 @@ public class BinarySearchTest
         Assert.Equal(3, sut.Find(6));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Finds_a_value_at_the_beginning_of_an_array()
     {
         var array = new[] { 1, 3, 4, 6, 8, 9, 11 };
@@ -28,7 +28,7 @@ public class BinarySearchTest
         Assert.Equal(0, sut.Find(1));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Finds_a_value_at_the_end_of_an_array()
     {
         var array = new[] { 1, 3, 4, 6, 8, 9, 11 };
@@ -36,7 +36,7 @@ public class BinarySearchTest
         Assert.Equal(6, sut.Find(11));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Finds_a_value_in_an_array_of_odd_length()
     {
         var array = new[] { 1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634 };
@@ -44,7 +44,7 @@ public class BinarySearchTest
         Assert.Equal(9, sut.Find(144));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Finds_a_value_in_an_array_of_even_length()
     {
         var array = new[] { 1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 };
@@ -52,7 +52,7 @@ public class BinarySearchTest
         Assert.Equal(5, sut.Find(21));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Identifies_that_a_value_is_not_included_in_the_array()
     {
         var array = new[] { 1, 3, 4, 6, 8, 9, 11 };
@@ -60,7 +60,7 @@ public class BinarySearchTest
         Assert.Equal(-1, sut.Find(7));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void A_value_smaller_than_the_arrays_smallest_value_is_not_included()
     {
         var array = new[] { 1, 3, 4, 6, 8, 9, 11 };
@@ -68,7 +68,7 @@ public class BinarySearchTest
         Assert.Equal(-1, sut.Find(0));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void A_value_larger_than_the_arrays_largest_value_is_not_included()
     {
         var array = new[] { 1, 3, 4, 6, 8, 9, 11 };
@@ -76,7 +76,7 @@ public class BinarySearchTest
         Assert.Equal(-1, sut.Find(13));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Nothing_is_included_in_an_empty_array()
     {
         var array = new int[0];
