f98ecc238c65852cc258b6a2a6da055c867b0b39
Cindy <syhndie2@gmail.com>
2018-04-15 20:25:13 -0500

    Completed SaddlePoints - finding all the saddle points (max of row and also min of column) in a given two-dimensional array

--- saddle-points/SaddlePoints.cs
+++ saddle-points/SaddlePoints.cs
@@ -4,31 +4,52 @@ using System.Linq;
 
 public class SaddlePoints
 {
+    private int[,] Values { get; set; }
+
     public SaddlePoints(int[,] values)
     {
-    }
+        Values = values;
+   }
 
     public IEnumerable<Tuple<int, int>> Calculate()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int[] rowMaxes = new int[Values.GetLength(0)];
+        for (int row = 0; row < Values.GetLength(0); row++)
+        {
+            rowMaxes[row] = int.MinValue;
+            for (int col = 0; col < Values.GetLength(1); col++)
+            {
+                if (Values[row, col] > rowMaxes[row])
+                {
+                    rowMaxes[row] = Values[row, col];
+                }
+            }
+        }
+
+        int[] colMins = new int[Values.GetLength(1)];
+        for (int col = 0; col < Values.GetLength(1); col++)
+        {
+            colMins[col] = int.MaxValue;
+            for (int row = 0; row < Values.GetLength(0); row++)
+            {
+                if (Values[row, col] < colMins[col])
+                {
+                    colMins[col] = Values[row, col];
+                }
+            }
+        }
+
+        var saddleCoordinates = new List<Tuple<int, int>>();
+        for (int row = 0; row < Values.GetLength(0); row++)
+        {
+            for (int col = 0; col < Values.GetLength(1); col++)
+            {
+                if (Values[row, col] == rowMaxes[row] && Values[row, col] == colMins[col])
+                {
+                    saddleCoordinates.Add(Tuple.Create(row, col));
+                }
+            }
+        }
+        return saddleCoordinates;
     }
 }
-
-//It's called a "saddle point" because it is greater than or equal to
-//every element in its row and less than or equal to every element in
-//its column.
-
-//[Fact]
-//public void Can_identify_multiple_saddle_points()
-//{
-//    var input = new[,]
-//    {
-//            { 4, 5, 4 },
-//            { 3, 5, 5 },
-//            { 1, 5, 4 }
-//        };
-//    var sut = new SaddlePoints(input);
-//    var actual = sut.Calculate();
-//    var expected = new[] { Tuple.Create(0, 1), Tuple.Create(1, 1), Tuple.Create(2, 1) };
-//    Assert.Equal(expected, actual);
-//}
