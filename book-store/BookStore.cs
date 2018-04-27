a4aab1624cc2f466b8048c2bceae01564b616358
Cindy <syhndie2@gmail.com>
2018-04-26 20:39:33 -0500

    second iteration of BookStore exercise  - removed some stray comments

--- book-store/BookStore.cs
+++ book-store/BookStore.cs
@@ -2,19 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 
-/*
-One copy of any of the five books costs $8.                   $8 per book       $8 for 1
-
-If, however, you buy two different books, you get a 5%        $7.6 per book     $15.2 for 2
-discount on those two books.
-
-If you buy 3 different books, you get a 10% discount.         $7.2 per book     $21.6 for 3
-
-If you buy 4 different books, you get a 20% discount.         $6.4 per book     $25.6 for 4
-
-If you buy all 5, you get a 25% discount.                     $6 per book       $30 for 5
-*/
-
 public static class BookStore
 {
     public static double Total(IEnumerable<int> books)
@@ -56,6 +43,5 @@ public static class BookStore
         }
        
         return (double)price;
-
     }
 }
