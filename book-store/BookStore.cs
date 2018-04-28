21e747371bfa0ddd68d48f624941ee1d2720790d
Cindy <syhndie2@gmail.com>
2018-04-27 20:00:13 -0500

    Made BookStore exercise somewhat more flexible, and did initial commit for Gigasecond exercise

--- book-store/BookStore.cs
+++ book-store/BookStore.cs
@@ -6,10 +6,36 @@ public static class BookStore
 {
     public static double Total(IEnumerable<int> books)
     {
+
+/*
+ * Note that change in discount between a set of 3 and a set of 4 is twice the change between a set
+ * of 4 and a set a 5.
+ * 
+ * If the discounts change, the adjustment code at the end of this program needs to change to 
+ * accomodate the new pricing structure
+*/
+        decimal basePrice = 8m;
+        Dictionary<int, decimal> discounts = new Dictionary<int, decimal>
+        {
+            [5] = 0.25m,
+            [4] = 0.2m,
+            [3] = 0.1m,
+            [2] = 0.05m
+        };
+
         List<int> booksList = books.ToList();
+
+        decimal priceOfFive = (basePrice - (basePrice * discounts[5])) * 5;
+        decimal priceOfFour = (basePrice - (basePrice * discounts[4])) * 4;
+        decimal priceOfThree = (basePrice - (basePrice * discounts[3])) * 3;
+        decimal priceOfTwo = (basePrice - (basePrice * discounts[2])) * 2;
+
         decimal price = 0;
+
         int countOfFives = 0;
+        int countOFFours = 0;
         int countOfThrees = 0;
+        int countOfTwos = 0;
 
         List<int> distinctBooks = booksList.Distinct().ToList();
 
@@ -18,30 +44,50 @@ public static class BookStore
         {
             if (countDistinct == 5)
             {
-                price += 30m;
+                price += priceOfFive;
                 countOfFives++;
             }
-            else if (countDistinct == 4) price += 25.6m;
+            else if (countDistinct == 4)
+            {
+                price += priceOfFour;
+                countOFFours++;
+            }
             else if (countDistinct == 3)
             {
-                price += 21.6m;
+                price += priceOfThree;
                 countOfThrees++;
             }
-            else if (countDistinct == 2) price += 15.2m;
-            else if (countDistinct == 1) price += 8m;
+            else if (countDistinct == 2)
+            {
+                price += priceOfTwo;
+                countOfTwos++;
+            }
+            else if (countDistinct == 1)
+            {
+                price += basePrice;
+            }
 
             foreach (int book in distinctBooks) booksList.Remove(book);
             distinctBooks = booksList.Distinct().ToList();
             countDistinct = distinctBooks.Count();
         }
 
-        while (countOfFives >0 && countOfThrees > 0)
+/*
+ * Because the change in discount between a set of 3 and a set of 4 is twice the change between a set
+ * of 4 and a set a 5, if there is a set of 5 and a set of 3, it is cheaper to turn that into 2 sets of 4
+ * 
+ * If the discounts change, the following code needs to change to accomodate the new pricing structure
+*/
+        while (countOfFives > 0 && countOfThrees > 0)
         {
-            price -= .4m;
+            price -= priceOfFive;
+            price -= priceOfThree;
+            price += 2 * priceOfFour;
             countOfFives--;
             countOfThrees--;
+            countOFFours += 2;
         }
-       
+
         return (double)price;
     }
 }
