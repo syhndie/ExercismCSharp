7618bb5af1d4fbd03770ee1da568438382a5b77e
Cindy <syhndie2@gmail.com>
2018-04-29 14:35:50 -0500

    initial commit for DifferenceOfSquares exercise

---
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max)
+    {
+        return 0;
+    }
+
+    public static int CalculateSumOfSquares(int max)
+    {
+        return 0;
+    }
+
+    public static int CalculateDifferenceOfSquares(int max)
+    {
+        return 0;
+    }
+}
