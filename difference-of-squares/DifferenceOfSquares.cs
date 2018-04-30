e33fb2ecdfdb04c70f08a16a74a5cea601bca1f6
Cindy <syhndie2@gmail.com>
2018-04-30 15:12:05 -0500

    first iteration for DifferenceOfSquaresTest exercise

--- difference-of-squares/DifferenceOfSquares.cs
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -1,19 +1,38 @@
 ﻿using System;
+using System.Linq;
+
+//Find the difference between the square of the sum and the sum of the squares of the first N natural numbers.
+
 
 public static class DifferenceOfSquares
 {
+    public static int[] CreateSeries(int max)
+    {
+        int[] series = new int[max];
+        for (int i = 0; i < max; i++)
+        {
+            series[i] = i + 1;
+        }
+        return series;
+    }
     public static int CalculateSquareOfSum(int max)
     {
-        return 0;
+        int[] series = CreateSeries(max);
+        return series.Sum() * series.Sum();
     }
 
     public static int CalculateSumOfSquares(int max)
     {
-        return 0;
+        int[] series = CreateSeries(max);
+        for (int i = 0; i < max; i++)
+        {
+            series[i] = series[i] * series[i];
+        }
+        return series.Sum();
     }
 
     public static int CalculateDifferenceOfSquares(int max)
     {
-        return 0;
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
     }
 }
