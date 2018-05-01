34f258f50e207b78cda86798e4f69e6c49aca57f
Cindy <syhndie2@gmail.com>
2018-05-01 15:44:30 -0500

    first iteration for Hamming exercise

--- hamming/Hamming.cs
+++ hamming/Hamming.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        return 0;
+        if (firstStrand.Length != secondStrand.Length) throw new ArgumentException();
+
+        var firstArray = firstStrand.ToCharArray();
+        var secondArray = secondStrand.ToCharArray();
+
+        int hamming = 0;
+
+        for (int i = 0; i < firstArray.Length; i++)
+        {
+            if (firstArray[i] != secondArray[i]) hamming++;
+        }
+
+        return hamming;
     }
 }
