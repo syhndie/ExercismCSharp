747d62a0f40f2a25c52cc11c407d9d8643c60997
Cindy <syhndie2@gmail.com>
2018-05-02 16:14:44 -0500

    initial commit for Grains exercise

---
+++ grains/Grains.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        return 0;
+    }
+
+    public static ulong Total()
+    {
+        return 0;
+    }
+}
