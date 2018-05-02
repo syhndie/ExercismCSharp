dfbbae5137c8d636ff97aa1cdf0ab6365e6d400f
Cindy <syhndie2@gmail.com>
2018-05-02 17:08:51 -0500

    first iteration of Grains exercise

--- grains/Grains.cs
+++ grains/Grains.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 
+//the nth term = ar^(n-1) = 1*2^(n-1) = 2^(n-1)
+//the sum of the first n terms of the series = a(1-r^n)/(1-r) = 1*(1-2^64)/(1-2) = 2^64 = ulong.maxvalue
+
+
 public static class Grains
 {
     public static ulong Square(int n)
     {
-        return 0;
+        if (n <= 0 || n > 64) throw new ArgumentOutOfRangeException();
+        return (ulong)Math.Pow(2, n-1);
     }
 
     public static ulong Total()
     {
-        return 0;
+        return ulong.MaxValue;
     }
 }
