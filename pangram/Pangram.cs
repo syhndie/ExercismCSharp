f433db8fa172b82739e05f47eeb0d662ea983379
Cindy <syhndie2@gmail.com>
2018-05-04 20:06:43 -0500

    first iteration of Pangram exercise

--- pangram/Pangram.cs
+++ pangram/Pangram.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Linq;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
+        var alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < alphabet.Length; i++)
+        {
+            if (!input.ToLower().ToCharArray().Contains(alphabet[i])) return false;
+        }
         return true;
     }
 }
