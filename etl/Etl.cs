f20206e6e63750979a0fdea5baeba0dbc49a537c
Cindy <syhndie2@gmail.com>
2018-05-04 20:19:45 -0500

    initial commit for Elt exercis

---
+++ etl/Etl.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+public static class Etl
+{
+    public static IDictionary<string, int> Transform(IDictionary<int, IList<string>> old)
+    {
+        return new Dictionary<string, int>();
+    }
+}
