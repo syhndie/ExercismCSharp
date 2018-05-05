30032c6010c2badba0a3f8982588d54fe0235bfe
Cindy <syhndie2@gmail.com>
2018-05-05 15:30:34 -0500

    initial commit for Etl exercise

--- etl/Etl.cs
+++ etl/Etl.cs
@@ -1,10 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class Etl
 {
     public static IDictionary<string, int> Transform(IDictionary<int, IList<string>> old)
     {
-        return new Dictionary<string, int>();
+        Dictionary<string, int> newDictionary = new Dictionary<string, int>();
+
+        foreach (int score in old.Keys)
+        {
+            foreach (string letter in old[score])
+            {
+                newDictionary.Add(letter.ToLower(), score);
+            }
+        }
+        return newDictionary;
     }
 }
