f1e974a40904811a18fc071a84f744551ad6e9ee
Cindy <syhndie2@gmail.com>
2018-05-08 18:55:52 -0500

    first iteration of Strain exercise

--- strain/Strain.cs
+++ strain/Strain.cs
@@ -1,15 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class Strain
 {
     public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        return collection;
+        foreach (var item in collection)
+        {
+            if (predicate(item)) yield return item;
+        }
     }
 
     public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        return collection;
+        foreach (var item in collection)
+        {
+            if (!predicate(item)) yield return item;
+        }
     }
 }
