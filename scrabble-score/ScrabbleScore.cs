501348f422e5ac0cb2799a6fbb6bee89be1346f2
Cindy <syhndie2@gmail.com>
2018-05-10 15:56:04 -0500

    first iteration of ScrabbleScore exercise - not including double and triple letter  bonuses, and double and triple word bonues

--- scrabble-score/ScrabbleScore.cs
+++ scrabble-score/ScrabbleScore.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
+enum Bonus
+{
+    Double = 2,
+    Triple = 3
+}
 
 public static class ScrabbleScore
 {
     public static int Score(string input)
     {
-        return 0;
-    }
+        Dictionary<char, int> letterValues = new Dictionary<char, int>
+        {
+            ['a'] = 1,
+            ['b'] = 3,
+            ['c'] = 3,
+            ['d'] = 2,
+            ['e'] = 1,
+            ['f'] = 4,
+            ['g'] = 2,
+            ['h'] = 4,
+            ['i'] = 1,
+            ['j'] = 8,
+            ['k'] = 5,
+            ['l'] = 1,
+            ['m'] = 3,
+            ['n'] = 1,
+            ['o'] = 1,
+            ['p'] = 3,
+            ['q'] = 10,
+            ['r'] = 1,
+            ['s'] = 1,
+            ['t'] = 1,
+            ['u'] = 1,
+            ['v'] = 4,
+            ['w'] = 4,
+            ['x'] = 8,
+            ['y'] = 4,
+            ['z'] = 10
+        };
+        return input.ToLower().ToCharArray().Sum(c => letterValues[c]);
+    }    
 }
