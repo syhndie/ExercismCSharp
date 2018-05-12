80facf35ff5fa53239d1b158dc00f0ade4bdb33a
Cindy <syhndie2@gmail.com>
2018-05-12 11:56:21 -0500

    initial commit for RobotName exercise

---
+++ robot-name/RobotName.cs
@@ -0,0 +1,19 @@
+using System;
+
+//two uppercase letters followed by three digits
+
+public class Robot
+{
+    public string Name
+    {
+        get
+        {
+            return "";            
+        }
+    }
+
+    public void Reset()
+    {
+        
+    }
+}
