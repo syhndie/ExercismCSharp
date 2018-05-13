8a9c1de23fbbf72945151732e5a9678a44e2ae09
Cindy <syhndie2@gmail.com>
2018-05-13 18:55:04 -0500

    First iteration of RobotNames exercise

--- robot-name/RobotName.cs
+++ robot-name/RobotName.cs
@@ -1,19 +1,47 @@
 using System;
-
-//two uppercase letters followed by three digits
+using System.Linq;
+using System.Collections.Generic;
 
 public class Robot
 {
-    public string Name
+    private const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static Random random = new Random();
+    public static HashSet<string> allNames = new HashSet<string>();
+
+    private string _name;
+
+    public string Name 
     {
         get
         {
-            return "";            
+            return _name;
         }
+    }
+
+    public Robot()
+    {
+        _name = GetName();
     }
 
     public void Reset()
     {
-        
+        allNames.Remove(_name);
+        _name = GetName();        
+    }
+
+    private string GetName()
+    {
+        int i = 0;
+        while (i < 1000)
+        {
+            string name = $"{letters[random.Next(0, 26)]}{letters[random.Next(0,26)]}{random.Next(0,10)}{random.Next(0,10)}{random.Next(0,10)}";
+            if (allNames.Contains(name)) i++;
+            else
+            {
+                allNames.Add(name);
+                return name;
+            }
+        }
+        throw new TimeoutException("It took too long to assign a name to the robot.");
     }
 }
--- robot-name/RobotNameTest.cs
+++ robot-name/RobotNameTest.cs
@@ -1,4 +1,5 @@
 using Xunit;
+using System;
 
 public class RobotNameTest
 {
