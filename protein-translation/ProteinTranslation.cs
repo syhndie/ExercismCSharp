675a156382774a8361db5fa3bfc259d99d4b0705
Cindy <syhndie2@gmail.com>
2018-05-15 18:36:41 -0500

    third iteration of ProteinTranslation exercise

--- protein-translation/ProteinTranslation.cs
+++ protein-translation/ProteinTranslation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public enum Proteins
+public enum AminoAcid
 {
     Methionine,
     Phenylalanine,
@@ -16,41 +16,41 @@ public enum Proteins
 
 public static class ProteinTranslation
 {
-    private static readonly Dictionary<string, Proteins> mapCodonProtein = new Dictionary<string, Proteins>
+    private static readonly Dictionary<string, AminoAcid> mapCodons = new Dictionary<string, AminoAcid>
     {
-        ["AUG"] = Proteins.Methionine,
-        ["UUU"] = Proteins.Phenylalanine,
-        ["UUC"] = Proteins.Phenylalanine,
-        ["UUA"] = Proteins.Leucine,
-        ["UUG"] = Proteins.Leucine,
-        ["UCU"] = Proteins.Serine,
-        ["UCC"] = Proteins.Serine,
-        ["UCA"] = Proteins.Serine,
-        ["UCG"] = Proteins.Serine,
-        ["UAU"] = Proteins.Tyrosine,
-        ["UAC"] = Proteins.Tyrosine,
-        ["UGU"] = Proteins.Cysteine,
-        ["UGC"] = Proteins.Cysteine,
-        ["UGG"] = Proteins.Tryptophan,
-        ["UAA"] = Proteins.STOP,
-        ["UAG"] = Proteins.STOP,
-        ["UGA"] = Proteins.STOP
+        ["AUG"] = AminoAcid.Methionine,
+        ["UUU"] = AminoAcid.Phenylalanine,
+        ["UUC"] = AminoAcid.Phenylalanine,
+        ["UUA"] = AminoAcid.Leucine,
+        ["UUG"] = AminoAcid.Leucine,
+        ["UCU"] = AminoAcid.Serine,
+        ["UCC"] = AminoAcid.Serine,
+        ["UCA"] = AminoAcid.Serine,
+        ["UCG"] = AminoAcid.Serine,
+        ["UAU"] = AminoAcid.Tyrosine,
+        ["UAC"] = AminoAcid.Tyrosine,
+        ["UGU"] = AminoAcid.Cysteine,
+        ["UGC"] = AminoAcid.Cysteine,
+        ["UGG"] = AminoAcid.Tryptophan,
+        ["UAA"] = AminoAcid.STOP,
+        ["UAG"] = AminoAcid.STOP,
+        ["UGA"] = AminoAcid.STOP
     };
  
-    public static string[] Translate(string codon)
+    public static string[] Translate(string strand)
     {
-        return Enumerable.Range(0, codon.Length / 3)
-            .Select(i => codon.Substring(i * 3, 3))
-            .Select(s => GetProteinName(s))
-            .TakeWhile(s => s != $"{Proteins.STOP}")
+        return Enumerable.Range(0, strand.Length / 3)
+            .Select(i => strand.Substring(i * 3, 3))
+            .Select(s => GetAminoAcidName(s))
+            .TakeWhile(s => s != $"{AminoAcid.STOP}")
             .ToArray();
     }
 
-    private static string GetProteinName(string codon)
+    private static string GetAminoAcidName(string codon)
     {
-        if (mapCodonProtein.TryGetValue(codon, out Proteins protein))
+        if (mapCodons.TryGetValue(codon, out AminoAcid aminoAcid))
         {
-            return protein.ToString();
+            return aminoAcid.ToString();
         }
 
         throw new Exception("Invalid codon");
