6dee07551984ea62af4b49e8d63ff77af91beed4
Cindy <syhndie2@gmail.com>
2018-05-19 07:39:01 -0500

    first iteration of PerfectNumbers exercise

--- perfect-numbers/PerfectNumbers.cs
+++ perfect-numbers/PerfectNumbers.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public enum Classification
 {
@@ -11,6 +13,12 @@ public static class PerfectNumbers
 {
     public static Classification Classify(int number)
     {
-        throw new NotImplementedException();
+        if (number < 1) throw new ArgumentOutOfRangeException();
+
+        var aliqot = Enumerable.Range(1, number - 1).Where(i => number % i == 0).Sum();
+
+        if (aliqot < number) return Classification.Deficient;
+        if (aliqot > number) return Classification.Abundant;
+        return Classification.Perfect;
     }
 }
