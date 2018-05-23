a97541f71b9b15e2fdab3a9e5542369e88a41425
Cindy <syhndie2@gmail.com>
2018-05-23 12:55:47 -0500

    first iteration of Sieve exercise

--- sieve/Sieve.cs
+++ sieve/Sieve.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Sieve
 {
     public static int[] Primes(int limit)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (limit < 2) throw new ArgumentOutOfRangeException();
+
+        List<int> multiples = new List<int>();
+
+        var range = Enumerable.Range(2, limit - 1);
+
+        foreach (int number in range)
+        {
+           for (int i = 2; number * i <= limit; i++)
+            {
+                multiples.Add(number * i);                
+            }
+        }
+
+        return range.Except(multiples).ToArray();
     }
 }
