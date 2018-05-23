d7e818fc321fcf58c330fc32a505dce3ae2d009f
Cindy <syhndie2@gmail.com>
2018-05-23 14:51:21 -0500

    first iteration of QueenAttack exercise

--- queen-attack/QueenAttack.cs
+++ queen-attack/QueenAttack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 public class Queen
 {
@@ -16,11 +17,17 @@ public static class QueenAttack
 {
     public static bool CanAttack(Queen white, Queen black)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (white.Row == black.Row) return true;
+        if (white.Column == black.Column) return true;
+        if (Math.Abs(white.Column - black.Column) == Math.Abs(white.Row - black.Row)) return true;
+        else return false;
     }
 
     public static Queen Create(int row, int column)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var allowable =Enumerable.Range(0, 8);
+
+        if (allowable.Contains(row) && allowable.Contains(column)) return new Queen(row, column);
+        else throw new ArgumentOutOfRangeException();
     }
 }
