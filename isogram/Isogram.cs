a46f34b0c4d51977c41b9b7867da3e8776cc563d
Cindy <syhndie2@gmail.com>
2018-05-26 15:38:01 -0500

    first iteration of Isogram exercise

--- isogram/Isogram.cs
+++ isogram/Isogram.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 public static class Isogram
 {
     public static bool IsIsogram(string word)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<char> hypenOrWhite = new List<char> { '-', ' ' };
+
+        var charList = word.ToLower().ToList();
+        charList.RemoveAll(c => hypenOrWhite.Contains(c));
+
+        List<char> allowable = new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        if (charList.Any(c => !allowable.Contains(c))) throw new ArgumentOutOfRangeException();
+
+        if (charList.Count() == charList.Distinct().Count()) return true;        
+        return false;
     }
 }
--- isogram/IsogramTest.cs
+++ isogram/IsogramTest.cs
@@ -1,6 +1,7 @@
 // This file was auto-generated based on version 1.2.0 of the canonical data.
 
 using Xunit;
+using System;
 
 public class IsogramTest
 {
@@ -10,51 +11,57 @@ public class IsogramTest
         Assert.True(Isogram.IsIsogram(""));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Isogram_with_only_lower_case_characters()
     {
         Assert.True(Isogram.IsIsogram("isogram"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Word_with_one_duplicated_character()
     {
         Assert.False(Isogram.IsIsogram("eleven"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Longest_reported_english_isogram()
     {
         Assert.True(Isogram.IsIsogram("subdermatoglyphic"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Word_with_duplicated_character_in_mixed_case()
     {
         Assert.False(Isogram.IsIsogram("Alphabet"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Hypothetical_isogrammic_word_with_hyphen()
     {
         Assert.True(Isogram.IsIsogram("thumbscrew-japingly"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Isogram_with_duplicated_hyphen()
     {
         Assert.True(Isogram.IsIsogram("six-year-old"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Made_up_name_that_is_an_isogram()
     {
         Assert.True(Isogram.IsIsogram("Emily Jung Schwartzkopf"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Duplicated_character_in_the_middle()
     {
         Assert.False(Isogram.IsIsogram("accentor"));
+    }
+
+    [Fact]
+    public void Invalid_characters_throw_exception()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => Isogram.IsIsogram("isogram2"));
     }
 }
