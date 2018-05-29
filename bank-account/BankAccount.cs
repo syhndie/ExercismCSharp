1a9eaee9e6ee39c79a4f405bec86b0e669b7ea7f
Cindy <syhndie2@gmail.com>
2018-05-29 15:58:49 -0500

    initial commit for BankAccount exercise

---
+++ bank-account/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+
+public class BankAccount
+{
+    private float _balance;
+
+    public void Open()
+    {
+        _balance = 0; 
+    }
+
+    public void Close()
+    {
+        
+    }
+
+    public float Balance
+    {
+        get
+        {
+            return _balance;
+        }
+    }
+
+    public void UpdateBalance(float change)
+    {
+
+    }
+}
