807f35e6a45c482472052338d0c42ca0507a4894
Cindy <syhndie2@gmail.com>
2018-05-29 20:29:39 -0500

    first iteration of BankAccount exercise

--- bank-account/BankAccount.cs
+++ bank-account/BankAccount.cs
@@ -1,31 +1,38 @@
 ﻿using System;
-
-
+using System.Threading.Tasks;
 
 public class BankAccount
 {
     private float _balance;
+    private bool IsOpen;
+    private object _lock = new object();
 
     public void Open()
     {
+        IsOpen = true;
         _balance = 0; 
     }
 
     public void Close()
     {
-        
+        IsOpen = false;        
     }
 
     public float Balance
     {
         get
         {
-            return _balance;
+            if (IsOpen) return _balance;
+            throw new InvalidOperationException("This account is closed and may not be accessed.");
         }
     }
 
     public void UpdateBalance(float change)
     {
-
+        lock (_lock)
+        {
+            if (IsOpen) _balance = _balance + change;
+            else throw new InvalidOperationException("This account is closed and may not be accessed.");
+        }
     }
 }
