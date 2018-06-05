bcb5b1bd66026d62d840d4d76a36aba9a540d03b
Cindy <syhndie2@gmail.com>
2018-06-05 12:18:54 -0500

    first iteration of Matrix exercise

--- matrix/Matrix.cs
+++ matrix/Matrix.cs
@@ -1,16 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MatrixElement
+{
+    public int Value { get; set; }
+    public int Row { get; set; }
+    public int Col { get; set; }
+}
 
 public class Matrix
 {
+    private List<MatrixElement> elements;
+
     public Matrix(string input)
     {
+        elements = new List<MatrixElement>();
+        int elementInt = 0;
+        int row = 0;
+        int col = 0;
+
+        string[] rowArray = input.Split('\n');
+
+        foreach (string rowString in rowArray)
+        {
+            string[] elementArray = rowString.Split(" ");
+
+            foreach (string elementString in elementArray)
+            {
+                if (int.TryParse(elementString, out elementInt))
+                {
+                    MatrixElement element = new MatrixElement
+                    {
+                        Value = elementInt,
+                        Row = row,
+                        Col = col
+                    };
+
+                    elements.Add(element);
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid input. Only integer values are allowed.");
+                }
+
+                col++;
+            }
+
+            col = 0;
+            row++;
+        }        
     }
 
     public int Rows
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            return elements.Max(e => e.Row) + 1;
         }
     }
 
@@ -18,17 +64,37 @@ public class Matrix
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            return elements.Max(e => e.Col) + 1;
         }
     }
 
     public int[] Row(int row)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int[] rowArray = new int[Cols];
+        for (int i = 0; i < Cols; i++)
+        {
+            rowArray[i] = elements
+                .Where(e => e.Row == row)
+                .Where(e => e.Col == i)
+                .Select(e => e.Value)
+                .Single();
+        }
+
+        return rowArray;
     }
 
     public int[] Column(int col)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int[] colArray = new int[Rows];
+        for (int i = 0; i < Rows; i++)
+        {
+            colArray[i] = elements
+                .Where(e => e.Col == col)
+                .Where(e => e.Row == i)
+                .Select(e => e.Value)
+                .Single();
+        }
+
+        return colArray;
     }
 }
