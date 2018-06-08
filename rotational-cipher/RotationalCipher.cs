59c44f67f928e178f16b5ed8dac8b477f01a4da6
Cindy <syhndie2@gmail.com>
2018-06-07 20:05:54 -0500

    first iteration of RotationalCipher exercise

--- rotational-cipher/RotationalCipher.cs
+++ rotational-cipher/RotationalCipher.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class RotationalCipher
 {
     public static string Rotate(string text, int shiftKey)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (!Char.IsLetter(text[i]))
+            {
+                result.Append(text[i]);
+                continue;
+            }
+
+            int intValueOfOrigChar = Convert.ToInt32(text[i]);
+            int intValueShifted = intValueOfOrigChar + shiftKey;
+
+            if (intValueOfOrigChar <= 90)
+            {
+                if (intValueShifted > 90) intValueShifted = intValueShifted - 26;
+                else if (intValueShifted < 65) intValueShifted = intValueShifted + 26;
+            }
+            else
+            {
+                if (intValueShifted > 122) intValueShifted = intValueShifted - 26;
+                else if (intValueShifted < 97) intValueShifted = intValueShifted + 26;
+            }
+
+            char charShifted = Convert.ToChar(intValueShifted);
+            result.Append(charShifted);            
+        }
+
+        return result.ToString();
     }
 }
