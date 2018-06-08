dbf0b5733cdc4ce5e53fdef4b81f06f75b01298f
Cindy <syhndie2@gmail.com>
2018-06-08 15:53:09 -0500

    second iteration of RotationalCipher exercise

--- rotational-cipher/RotationalCipher.cs
+++ rotational-cipher/RotationalCipher.cs
@@ -7,33 +7,25 @@ public static class RotationalCipher
 {
     public static string Rotate(string text, int shiftKey)
     {
-        StringBuilder result = new StringBuilder();
+        string result = string.Empty;
+        int? shifted = null;
+
         for (int i = 0; i < text.Length; i++)
         {
-            if (!Char.IsLetter(text[i]))
+            if (Char.IsLetter(text[i]))
             {
-                result.Append(text[i]);
-                continue;
+                shifted = text[i] + shiftKey;
+
+                if (Char.IsUpper(text[i]) && shifted > 'Z') shifted -= 26;
+
+                else if (Char.IsLower(text[i]) && shifted > 'z') shifted -= 26;
             }
 
-            int intValueOfOrigChar = Convert.ToInt32(text[i]);
-            int intValueShifted = intValueOfOrigChar + shiftKey;
+            else shifted = text[i];
 
-            if (intValueOfOrigChar <= 90)
-            {
-                if (intValueShifted > 90) intValueShifted = intValueShifted - 26;
-                else if (intValueShifted < 65) intValueShifted = intValueShifted + 26;
-            }
-            else
-            {
-                if (intValueShifted > 122) intValueShifted = intValueShifted - 26;
-                else if (intValueShifted < 97) intValueShifted = intValueShifted + 26;
-            }
-
-            char charShifted = Convert.ToChar(intValueShifted);
-            result.Append(charShifted);            
+            result += (char)shifted;       
         }
 
-        return result.ToString();
+        return result;
     }
 }
