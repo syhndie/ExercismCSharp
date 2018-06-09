502bf1057043020c04f836157061891b1cd6f46e
Cindy <syhndie2@gmail.com>
2018-06-09 10:33:40 -0500

    initial commit for House exercise

---
+++ house/House.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class House
+{
+    public static string Recite(int verseNumber)
+    {
+        return "";
+    }
+
+    public static string Recite(int startVerse, int endVerse)
+    {
+        return "";
+    }
+}
