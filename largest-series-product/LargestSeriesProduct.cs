c7638cdb60bc0fac443903d4dc86216e785c198a
Cindy <syhndie2@gmail.com>
2018-06-08 19:02:38 -0500

    first iteration of LargestSeriesProduct exercise

--- largest-series-product/LargestSeriesProduct.cs
+++ largest-series-product/LargestSeriesProduct.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Linq;
 
 public static class LargestSeriesProduct
 {
     public static long GetLargestProduct(string digits, int span) 
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (span < 0 || digits.Length < span) throw new ArgumentException();
+        if (digits.Any(c => !Char.IsDigit(c))) throw new ArgumentException();
+
+        int maxProduct = 0;
+
+        for (int i = 0; i <= digits.Length - span; i++)
+        {
+            var newSegment = digits.Substring(i, span);
+
+            var newProduct = 1;
+            for (int j = 0; j < newSegment.Length; j++)
+            {
+                newProduct = newProduct * int.Parse(newSegment[j].ToString());
+            }
+
+            if (newProduct > maxProduct) maxProduct = newProduct;
+        }
+        return maxProduct; ;
     }
 }
