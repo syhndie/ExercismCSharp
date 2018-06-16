0cd438ecbb94f1b5f07d692f5172f683c0215c49
Cindy <syhndie2@gmail.com>
2018-06-15 20:50:10 -0500

    second iteration of LargestSeriesProduct exercise

--- largest-series-product/LargestSeriesProduct.cs
+++ largest-series-product/LargestSeriesProduct.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 public static class LargestSeriesProduct
 {
@@ -8,21 +9,17 @@ public static class LargestSeriesProduct
         if (span < 0 || digits.Length < span) throw new ArgumentException();
         if (digits.Any(c => !Char.IsDigit(c))) throw new ArgumentException();
 
-        int maxProduct = 0;
+        return Enumerable
+            .Range(0, digits.Length - span + 1)
+            .Select(i => digits.Substring(i, span))
+            .Select(s => GetProductFromSegment(s))
+            .Max();
+    }
 
-        for (int i = 0; i <= digits.Length - span; i++)
-        {
-            var newSegment = digits.Substring(i, span);
-
-            var newProduct = 1;
-            for (int j = 0; j < newSegment.Length; j++)
-            {
-                newProduct = newProduct * int.Parse(newSegment[j].ToString());
-            }
-
-            if (newProduct > maxProduct) maxProduct = newProduct;
-        }
-
-        return maxProduct;
+    private static int GetProductFromSegment (string segment)
+    {
+        return (int)segment
+            .Select(c => Char.GetNumericValue(c))
+            .Aggregate(1d, (acc, digit) => acc * digit);
     }
 }
