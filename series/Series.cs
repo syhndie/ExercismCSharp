162340297a62f6845ab1c20538210f22a20fa51f
Cindy <syhndie2@gmail.com>
2018-06-18 15:43:36 -0500

    first iteration of Series exercise

--- series/Series.cs
+++ series/Series.cs
@@ -1,13 +1,30 @@
 using System;
+using System.Linq;
 
 public class Series
 {
+    private string numberString;
+
     public Series(string numbers)
     {
+        numberString = numbers;
     }
 
     public int[][] Slices(int sliceLength)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (numberString.Length < sliceLength) throw new ArgumentException();
+
+        return Enumerable
+            .Range(0, numberString.Length - sliceLength + 1)
+            .Select(i => numberString.Substring(i, sliceLength))
+            .Select(s => GetIntArray(s))
+            .ToArray();
+    }
+
+    private int[] GetIntArray(string subString)
+    {
+        return subString
+            .Select(c => (int)Char.GetNumericValue(c))
+            .ToArray();
     }
 }
