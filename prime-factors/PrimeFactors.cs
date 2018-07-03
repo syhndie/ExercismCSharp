0f25ee074d21fdd732dfbe6058e9a4273b95f455
Cindy <syhndie2@gmail.com>
2018-07-03 16:04:56 -0500

    first iteration of PrimeFactors exercise

--- prime-factors/PrimeFactors.cs
+++ prime-factors/PrimeFactors.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 public static class PrimeFactors
 {
     public static int[] Factors(long number)
     {
-        throw new NotImplementedException();
+        List<int> primeFactors = new List<int>();
+
+        for(int i = 2; number > 1; i++)
+        {
+            while (number % i == 0)
+            {
+                primeFactors.Add(i);
+                number /= i;
+            }
+        }
+        return primeFactors.ToArray();
     }
 }
