45e34565daf1fb84e79df052abaa2eace4c90442
Cindy <syhndie2@gmail.com>
2018-07-06 19:09:28 -0500

    first iteration of Meetup exercise

--- meetup/Meetup.cs
+++ meetup/Meetup.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public enum Schedule
 {
@@ -12,12 +14,33 @@ public enum Schedule
 
 public class Meetup
 {
+    private readonly DateTime[] _calendar;
+
     public Meetup(int month, int year)
     {
+        int daysInMonth = DateTime.DaysInMonth(year, month);
+
+        _calendar = Enumerable.Range(1, daysInMonth).Select(i => new DateTime(year, month, i)).ToArray();
     }
 
     public DateTime Day(DayOfWeek dayOfWeek, Schedule schedule)
     {
-        return DateTime.Today;
+        DateTime[] daysOfInterest = _calendar.Where(d => d.DayOfWeek == dayOfWeek).ToArray();
+
+        switch (schedule)
+        {
+            case Schedule.Teenth:
+                return daysOfInterest.Single(d => d.Day > 12 && d.Day < 20);
+            case Schedule.First:
+                return daysOfInterest[0];
+            case Schedule.Second:
+                return daysOfInterest[1];
+            case Schedule.Third:
+                return daysOfInterest[2];
+            case Schedule.Fourth:
+                return daysOfInterest[3];
+            default:
+                return daysOfInterest.Last();
+        }
     }
 }
