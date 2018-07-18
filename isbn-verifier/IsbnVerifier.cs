79c66cb3837c49c9ea6f1799ba465d94169ef887
Cindy <syhndie2@gmail.com>
2018-07-17 20:30:00 -0500

    first iteration of IsbnVerifier exercise

--- isbn-verifier/IsbnVerifier.cs
+++ isbn-verifier/IsbnVerifier.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 public static class IsbnVerifier
 {
     public static bool IsValid(string number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var numberNoHyphens = number.Replace("-", "");
+
+        if (!Regex.IsMatch(numberNoHyphens, @"^([\d]{9})[X\d]$")) return false;
+
+        var intArray = numberNoHyphens
+            .ToCharArray()
+            .Select(c => c != 'X' ? (int)c - 48 : 10)
+            .ToArray();
+
+        var sumOfProducts = Enumerable.Range(1, 10)
+            .Reverse()
+            .Select(i => i * intArray[i - 1])
+            .Sum();
+
+        return sumOfProducts % 11 == 0;
     }
 }
+
+//(x1 * 10  +  x2 * 9  +  x3 * 8  +  x4 * 7  +  x5 * 6  +  x6 * 5  +  x7 * 4  +  x8 * 3  +  x9 * 2  +  x10 * 1)  mod 11  ==  0
