70534cc8913bdb0e06f13fd084463fc4116914fb
Cindy <syhndie2@gmail.com>
2018-07-24 15:23:53 -0500

    first iteration of ListOps exercise

--- list-ops/ListOps.cs
+++ list-ops/ListOps.cs
@@ -6,41 +6,91 @@ public static class ListOps
 {
     public static int Length<T>(List<T> input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int count = 0;
+
+        foreach (T item in input)
+        {
+            count++;
+        }
+        return count;
     }
 
     public static List<T> Reverse<T>(List<T> input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<T> outList = new List<T>();
+
+        for (int i = Length(input) - 1; i >= 0; i--)
+        {
+            outList.Add(input[i]);
+        }
+
+        return outList;
     }
 
     public static List<TOut> Map<TIn, TOut>(List<TIn> input, Func<TIn, TOut> map)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<TOut> list = new List<TOut>();
+
+        foreach (TIn item in input)
+        {
+            TOut newItem = map(item);
+            list.Add(newItem);
+        }
+
+        return list;
     }
 
     public static List<T> Filter<T>(List<T> input, Func<T, bool> predicate)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<T> list = new List<T>();
+        foreach (T item in input)
+        {
+            if (predicate(item)) list.Add(item);
+        }
+
+        return list;
     }
 
     public static TOut Foldl<TIn, TOut>(List<TIn> input, TOut start, Func<TOut, TIn, TOut> func)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var accumulator = start;
+
+        foreach (TIn item in input)
+        {
+            accumulator = func(accumulator, item);
+        }
+
+        return accumulator;
     }
 
     public static TOut Foldr<TIn, TOut>(List<TIn> input, TOut start, Func<TIn, TOut, TOut> func)
     {
-        throw new NotImplementedException("You need to implement this function.");
+
+        List<TIn> reversedList = Reverse(input);
+
+        var accumulator = start;
+
+        foreach (TIn item in reversedList)
+        {
+            accumulator = func(item, accumulator);
+        }
+
+        return accumulator;
     }
 
     public static List<T> Concat<T>(List<List<T>> input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<T> list = new List<T>();
+        return Foldl(input, list, (x, y) => Append(x, y));
     }
 
     public static List<T> Append<T>(List<T> left, List<T> right)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        foreach (T item in right)
+        {
+            left.Add(item);
+        }
+
+        return left;
     }
 }
