c071bd9214a0e482a722bcf32a662c1d35526796
Cindy <syhndie2@gmail.com>
2018-08-10 20:53:47 -0500

    first iteration of FlattenArray exercise

--- flatten-array/FlattenArray.cs
+++ flatten-array/FlattenArray.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 
 public static class FlattenArray
 {
     public static IEnumerable Flatten(IEnumerable input)
     {
-        return new int[] { };
+        foreach (var i in input)
+        {
+            if (i == null) continue;
+
+            if (i is IEnumerable)
+            {
+                foreach (var j in Flatten(i as IEnumerable)) yield return j;
+            }
+
+            else yield return i;
+        }
     }
 }
