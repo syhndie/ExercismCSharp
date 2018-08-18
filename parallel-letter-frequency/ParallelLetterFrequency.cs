f59fbaa8f5c60b24495693bf3ecc39f1fed14908
Cindy <syhndie2@gmail.com>
2018-08-18 16:19:48 -0500

    first iteration of ParallelLetterFrequency exercise

--- parallel-letter-frequency/ParallelLetterFrequency.cs
+++ parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Concurrent;
 
 public static class ParallelLetterFrequency
 {
     public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
     {
-        throw new NotImplementedException();
+        ConcurrentDictionary<char, int> result = new ConcurrentDictionary<char, int>();
+
+        Parallel.ForEach(texts, s =>
+        {
+            var letterCounts = s.ToLower()
+                .Where(char.IsLetter)
+                .GroupBy(c => c);
+
+            foreach (var g in letterCounts)
+            {
+                result.AddOrUpdate(g.Key, g.Count(), (k, v) => v + g.Count());
+            }
+        });
+
+        return result.ToDictionary(p => p.Key, p => p.Value);
     }
 }
