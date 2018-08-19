53455804058d6fbd844c32e0665e597bfd07c317
Cindy <syhndie2@gmail.com>
2018-08-19 18:04:25 -0500

    first iteration of AtbashCipher exercise

--- atbash-cipher/AtbashCipher.cs
+++ atbash-cipher/AtbashCipher.cs
@@ -1,14 +1,29 @@
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class AtbashCipher
 {
     public static string Encode(string plainValue)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        char[] encodedChars = plainValue
+            .ToLower()
+            .Where(c => Char.IsLetterOrDigit(c))
+            .Select(c => Char.IsDigit(c) ? c : (char)( 'z' - ( c - 'a')))
+            .ToArray();
+
+        string encodedStringWithoutSpaces = new string(encodedChars);
+
+        return Regex.Replace(encodedStringWithoutSpaces, ".{5}", "$0 ").Trim();
     }
 
     public static string Decode(string encodedValue)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        char[] decodedChars = encodedValue
+            .Where(c => Char.IsLetterOrDigit(c))
+            .Select(c => Char.IsDigit(c) ? c : (char)('z' - c + 'a'))
+            .ToArray();
+
+        return new string(decodedChars);
     }
 }
