281f835895b7752e6dd6354f2cfd959c4b71832b
Cindy <syhndie2@gmail.com>
2018-09-12 10:48:34 -0500

    first iteration of ScaleGenerator exercise

--- scale-generator/ScaleGenerator.cs
+++ scale-generator/ScaleGenerator.cs
@@ -1,14 +1,73 @@
 ﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 
 public static class ScaleGenerator
 {
     public static string[] Pitches(string tonic)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Pitches(tonic, "mmmmmmmmmmmm");
     }
 
     public static string[] Pitches(string tonic, string pattern)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        //intervals are m = minor second, which steps to the next note
+        //              M = major second, which steps to the second next note
+       //               A = augmented second, which steps the the third next note
+        Dictionary<char, int> intervals = new Dictionary<char, int>
+        {
+            {'m', 1 },
+            {'M', 2 },
+            {'A', 3 }
+        };
+
+        //these are the naturals (no sharps or flats) that have scales with flats when used as tonics
+        string[] naturalTonicsUSingFlats = new string[]
+        {
+            "c", "d", "f", "F", "g"
+        };
+
+        //all notes, using sharp notation
+        string[] notesWithSharps = new string[]
+        {
+            "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"
+        };
+
+        //all notes, using flat notation
+        string[] notesWithFlats = new string[]
+        {
+            "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"
+        };
+
+
+        //select which array of notes to use - sharps or flats
+        bool tonicHasFlat = tonic.Substring(1) == "b";
+        bool tonicIsNaturalUsingFlats = naturalTonicsUSingFlats.Contains(tonic);
+        string[] notesToUse = (tonicHasFlat || tonicIsNaturalUsingFlats)  ? notesWithFlats : notesWithSharps;
+
+        //find index of starting note in array of notes to use
+        int noteIndex = Array.IndexOf(notesToUse, notesToUse.Single(s => s.ToLower() == tonic.ToLower()));
+
+        //add first note to results
+        List<string> results = new List<string> { notesToUse[noteIndex] };
+
+        //use each interval in the pattern to choose next note to add to results
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            //incread note index by interval associated with character from the pattern
+            noteIndex += intervals[pattern[i]];
+
+            //loop back to beginning of notes if needed
+            if (noteIndex > notesToUse.Length - 1) noteIndex -= notesToUse.Length;
+
+            //add note to results
+            results.Add(notesToUse[noteIndex]);
+        }
+
+        //check that the interval to the last note gave the same note started with - remove that note from the results
+        if (results.First() == results.Last()) return results.Take(results.Count - 1).ToArray();
+        else throw new ArgumentException("Invalid pattern.");
     }
 }
+
--- scale-generator/ScaleGeneratorTest.cs
+++ scale-generator/ScaleGeneratorTest.cs
@@ -11,112 +11,112 @@ public class ScaleGeneratorTest
         Assert.Equal(expected, ScaleGenerator.Pitches("C"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Chromatic_scale_with_flats()
     {
         var expected = new[] { "F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E" };
         Assert.Equal(expected, ScaleGenerator.Pitches("F"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Simple_major_scale()
     {
         var expected = new[] { "C", "D", "E", "F", "G", "A", "B" };
         Assert.Equal(expected, ScaleGenerator.Pitches("C", "MMmMMMm"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Major_scale_with_sharps()
     {
         var expected = new[] { "G", "A", "B", "C", "D", "E", "F#" };
         Assert.Equal(expected, ScaleGenerator.Pitches("G", "MMmMMMm"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Major_scale_with_flats()
     {
         var expected = new[] { "F", "G", "A", "Bb", "C", "D", "E" };
         Assert.Equal(expected, ScaleGenerator.Pitches("F", "MMmMMMm"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Minor_scale_with_sharps()
     {
         var expected = new[] { "F#", "G#", "A", "B", "C#", "D", "E" };
         Assert.Equal(expected, ScaleGenerator.Pitches("f#", "MmMMmMM"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Minor_scale_with_flats()
     {
         var expected = new[] { "Bb", "C", "Db", "Eb", "F", "Gb", "Ab" };
         Assert.Equal(expected, ScaleGenerator.Pitches("bb", "MmMMmMM"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Dorian_mode()
     {
         var expected = new[] { "D", "E", "F", "G", "A", "B", "C" };
         Assert.Equal(expected, ScaleGenerator.Pitches("d", "MmMMMmM"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Mixolydian_mode()
     {
         var expected = new[] { "Eb", "F", "G", "Ab", "Bb", "C", "Db" };
         Assert.Equal(expected, ScaleGenerator.Pitches("Eb", "MMmMMmM"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Lydian_mode()
     {
         var expected = new[] { "A", "B", "C#", "D#", "E", "F#", "G#" };
         Assert.Equal(expected, ScaleGenerator.Pitches("a", "MMMmMMm"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Phrygian_mode()
     {
         var expected = new[] { "E", "F", "G", "A", "B", "C", "D" };
         Assert.Equal(expected, ScaleGenerator.Pitches("e", "mMMMmMM"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Locrian_mode()
     {
         var expected = new[] { "G", "Ab", "Bb", "C", "Db", "Eb", "F" };
         Assert.Equal(expected, ScaleGenerator.Pitches("g", "mMMmMMM"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Harmonic_minor()
     {
         var expected = new[] { "D", "E", "F", "G", "A", "Bb", "Db" };
         Assert.Equal(expected, ScaleGenerator.Pitches("d", "MmMMmAm"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Octatonic()
     {
         var expected = new[] { "C", "D", "D#", "F", "F#", "G#", "A", "B" };
         Assert.Equal(expected, ScaleGenerator.Pitches("C", "MmMmMmMm"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Hexatonic()
     {
         var expected = new[] { "Db", "Eb", "F", "G", "A", "B" };
         Assert.Equal(expected, ScaleGenerator.Pitches("Db", "MMMMMM"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Pentatonic()
     {
         var expected = new[] { "A", "B", "C#", "E", "F#" };
         Assert.Equal(expected, ScaleGenerator.Pitches("A", "MMAMA"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Enigmatic()
     {
         var expected = new[] { "G", "G#", "B", "C#", "D#", "F", "F#" };
