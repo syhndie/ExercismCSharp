1e732b8637f363239a52a3ef4e7a84968104a06b
Cindy <syhndie2@gmail.com>
2018-09-13 11:20:09 -0500

    first iteration of BracketPush exercise

--- bracket-push/BracketPush.cs
+++ bracket-push/BracketPush.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class BracketPush
 {
     public static bool IsPaired(string input)
     {
-        return true;
+        Dictionary<char, char> bracketPairs = new Dictionary<char, char>
+        {
+            { ')', '(' },
+            { '}', '{' },
+            { ']', '[' }
+        };
+
+        Stack<char> openBracketStack = new Stack<char>();
+
+        foreach (char c in input)
+        {
+            switch (c)
+            {
+                case '(':
+                case '{':
+                case '[':
+                    openBracketStack.Push(c);
+                    break;
+
+                case ')':
+                case '}':
+                case ']':
+                    if (openBracketStack.Count() == 0) return false;
+                    if (openBracketStack.Peek() == bracketPairs[c]) openBracketStack.Pop();
+                    else return false;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        return openBracketStack.Count() == 0;
     }
 }
