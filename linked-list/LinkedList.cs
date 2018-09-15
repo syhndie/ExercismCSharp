f516fe03535c4659aac56cae2f42b6dff9172b43
Cindy <syhndie2@gmail.com>
2018-09-15 08:31:58 -0500

    first iteration of LinkedList exercise

--- linked-list/LinkedList.cs
+++ linked-list/LinkedList.cs
@@ -1,24 +1,73 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Deque<T>
 {
+    public Node First { get; set; }
+    public Node Last { get; set; }
+
+    public class Node
+    {
+        public T Value { get; set; }
+        public Node Previous { get; set; }
+        public Node Next { get; set; }
+       
+        public Node (T value)
+        {
+            Value = value;
+            Previous = null;
+            Next = null;
+        }
+
+        public Node (T value, Node previous, Node next)
+        {
+            Value = value;
+            Previous = previous;
+            Next = next;
+        }
+    }       
+
     public void Push(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (First == null)
+        {
+            First = new Node(value);
+            Last = First;
+        }
+        else
+        {
+            Last.Next = new Node(value, Last, null);
+            Last = Last.Next;
+        }
     }
 
     public T Pop()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        T popped = Last.Value;
+        Last = Last.Previous;
+        return popped;
     }
 
     public void Unshift(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (First == null)
+        {
+            First = new Node(value);
+            Last = First;
+        }
+        else
+        {
+            First.Previous = new Node(value, null, First);
+            First = First.Previous;
+        }
     }
 
     public T Shift()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        T shifted = First.Value;
+        First = First.Next;
+        return shifted;
     }
 }
